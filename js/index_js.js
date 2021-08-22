b491f5b0111c141f32a11aaafa566f5b49a1b0b2
Kumar Ajay <kumar.a.ajay@pwc.com>
2021-08-22 12:43:02 +0530

    Only Registration Page Designed but not functional

---
+++ js/index_js.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".menus li a").click(function (e) {
+        let target = $(this).attr("href");
+        $("html,body").animate({
+            scrollTop: $(target).offset().top - 50,
+        }, 1000);
+        e.preventDefault();
+    });
+});
